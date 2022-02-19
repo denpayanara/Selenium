70f197c90eb76054d5c7d83b5909a8f06de06767
denpayanara <88641815+denpayanara@users.noreply.github.com>
2022-02-19 12:11:05 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import datetime
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+web_driver = webdriver.Chrome(options=options)
+
+# 野迫川ライブカメラ
+url_nosegawa = 'https://www.komadori.ne.jp/nara-livecamera/area/nosegawa1.html'
+
+web_driver.get(url_nosegawa)
+
+now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
+now_str = now.strftime('%Y%m%d_%H')
+
+web_driver.save_screenshot(f'{now_str}.png')
